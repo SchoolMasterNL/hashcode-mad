b8133575e731d1188abf32d24bc3db7b9e0fbc5a
Dirk Lemstra <dirk@git.imagemagick.org>
2018-03-01 19:39:51 +0100

    Added Vehicle class

---
+++ src/hashcode-mad/Vehicle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hashcode_mad
+{
+    internal class Vehicle
+    {
+        private List<Ride> rides;
+
+        public Vehicle(int id)
+        {
+            Id = id;
+            rides = new List<Ride>();
+        }
+
+        public int Id { get; }
+
+        public void AssignRide(Ride ride)
+        {
+            rides.Add(ride);
+        }
+
+        public int GetDistance(Ride ride)
+        {
+            int x = 0;
+            int y = 0;
+
+            if (rides.Count > 0)
+            {
+                var lastRide = rides[rides.Count - 1];
+                x = lastRide.EndX;
+                y = lastRide.EndY;
+            }
+
+            return Math.Abs(x + ride.StartX) + Math.Abs(y + ride.StartY);
+        }
+    }
+}
