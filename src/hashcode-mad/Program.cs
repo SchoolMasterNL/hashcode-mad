ee84d910257067b158a970b93e958b8b789c1fd4
Arjen Post <mail@arjenpost.nl>
2018-03-01 18:49:56 +0100

    empty project added

---
+++ src/hashcode-mad/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hashcode_mad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
